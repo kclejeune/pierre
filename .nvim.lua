d9eb0ab8e5c8e797520aecda0e1d9ba74e281ac8
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-08-12 14:50:47 -0700

    chore: fix .nvim.lua init file for tsgo

--- .nvim.lua
+++ .nvim.lua
@@ -17,3 +17,17 @@ vim.g.ale_fixers = {
 }
 
 vim.g.ale_fix_on_save = 1
+
+-- Hack to support tsgo until we can upgrade internally to 7
+local project_root = vim.fs.root(0, { "pnpm-lock.yaml" }) or vim.fn.getcwd()
+local tsgo = vim.fs.joinpath(project_root, "node_modules", ".bin", "tsgo")
+
+if vim.fn.executable(tsgo) == 1 then
+	vim.lsp.config("tsgo", {
+		cmd = { tsgo, "--lsp", "--stdio" },
+	})
+	vim.defer_fn(function()
+		vim.lsp.enable("tsgo", false)
+		vim.lsp.enable("tsgo")
+	end, 100)
+end
